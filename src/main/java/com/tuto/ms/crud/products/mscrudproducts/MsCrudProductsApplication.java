20ce598fa9ac9c5db2b75d3629162af727b1910c
Omarc <omar.cherigui.oc@gmail.com>
2023-10-27 23:10:22 +0200

    Initial commit

---
+++ src/main/java/com/tuto/ms/crud/products/mscrudproducts/MsCrudProductsApplication.java
@@ -0,0 +1,13 @@
+package com.tuto.ms.crud.products.mscrudproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCrudProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCrudProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuto/ms/crud/products/mscrudproducts/MsCrudProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.ms.crud.products.mscrudproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCrudProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
